9018035a5ea5b80a0c7bb5dbd387fbf7e33ab03e
suidonglei <suidonglei@jd.com>
2019-08-07 11:15:51 +0800

    error answer

--- src/main/java/learn/algorithm/sliding/MaxSlidingWindow.java
+++ src/main/java/learn/algorithm/sliding/MaxSlidingWindow.java
@@ -9,8 +9,8 @@ package learn.algorithm.sliding;
  * You can only see the k numbers in the window. Each time the sliding window moves right by one position. Return the max sliding window.
  *
  * possible solutions:
- * 1.sort O(n nlogn)
- * 2.without sort O(n)  in linear time
+ * 1.sort O(n ·logK) 大顶堆
+ * 2.without sort O(n ·1)  in linear time  deque
  *
  * @date 2019/8/618:39
  */
@@ -21,14 +21,51 @@ public class MaxSlidingWindow {
      * @param k
      * @return
      */
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        int[] returnArray = new int[nums.length/k + 1];
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        if(null == nums || nums.length == 0) return new int[]{};
+        int[] returnArray = new int[nums.length - (k -1)];
         int index1 = 0, index2 = 0, value1 = nums[0], value2 = nums[0];
+        if(nums.length > 1) {
+            index2 = 1;
+            value2 = nums[1];
+        }
+        int returnIndex = 0, firstIndex = 0;
         for(int i = 0; i < nums.length; i ++) {
+            if((i - firstIndex + 1) > k) {//达到窗口
+                returnArray[returnIndex] = value1;
+                returnIndex ++;
+                if(index1 <= firstIndex) {
+                    index1 = index2;
+                    value1 = value2;
+                    if(index1 < (nums.length - 1)) {
+                        index2 = index1 + 1;
+                        value2 = nums[index1 + 1];
+                    }
+                }
+                firstIndex ++;
+            }
             if(nums[i] >= nums[index1]) {
-
+                index1 = i;
+                value1 = nums[i];
+                if(index2 == index1) {
+                    if(index1 < (nums.length - 1)) {
+                        index2 = index1 + 1;
+                        value2 = nums[index1 + 1];
+                    }
+                }
+            } else if(nums[i] >= nums[index2]){
+                index2 = i;
+                value2 = nums[i];
+            }
+            if(i == nums.length - 1) {
+                returnArray[returnIndex] = value1;
             }
         }
-        return null;
+        return returnArray;
+    }
+
+    public static void main(String[] args) {
+        int[] intArray = new int[]{1, 3, 1, 2, 0, 5};
+        System.out.println(maxSlidingWindow(intArray, 3));
     }
 }
