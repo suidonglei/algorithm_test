0236a32cee8a8f27ca2ff67273ae103089442a49
suidonglei <suidonglei@jd.com>
2019-08-15 15:09:07 +0800

    min max

--- src/main/java/learn/algorithm/recursion/PowXn.java
+++ src/main/java/learn/algorithm/recursion/PowXn.java
@@ -84,4 +84,12 @@ class BitwiseOperation{
         }
         return result;
     }
+
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(-Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(-Integer.MIN_VALUE);
+        System.out.println(-(Integer.MIN_VALUE + 1));
+    }
 }
