ecd42040de9cffecd40d2038937a883d4f49553d
suidonglei <suidonglei@jd.com>
2019-08-20 18:30:40 +0800

    min max

---
+++ src/main/java/learn/algorithm/dfsbfs/GenerateParentheses.java
@@ -0,0 +1,21 @@
+package learn.algorithm.dfsbfs;
+
+import java.util.List;
+
+/**
+ * @author suidonglei
+ * @title: GenerateParentheses
+ * @projectName algorithm-test
+ * @description: 22
+ * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ *
+ * possible solutions:
+ * recursion:
+ *
+ * @date 2019/8/2016:08
+ */
+public class GenerateParentheses {
+    public List<String> generateParenthesis(int n) {
+
+    }
+}
