7214f4d2830818b8e9bca1e688fd907aad1a6b63
suidonglei <suidonglei@jd.com>
2019-08-21 18:44:59 +0800

    min max

--- src/main/java/learn/algorithm/dfsbfs/GenerateParentheses.java
+++ src/main/java/learn/algorithm/dfsbfs/GenerateParentheses.java
@@ -1,21 +1,47 @@
 package learn.algorithm.dfsbfs;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @author suidonglei
  * @title: GenerateParentheses
  * @projectName algorithm-test
- * @description: 22
- * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ * @description: 22 Given n pairs of parentheses, write a function to generate all combinations of
+ * well-formed parentheses.
  *
- * possible solutions:
- * recursion:
- *
+ * possible solutions: Approach 1: Brute Force:  2^2n
  * @date 2019/8/2016:08
  */
 public class GenerateParentheses {
-    public List<String> generateParenthesis(int n) {
 
+  /**
+   * Brute Force
+   */
+  public List<String> generateParenthesis(int n) {
+    List<String> returnList = new ArrayList<>();
+    if (n < 1) {
+      return returnList;
     }
+    generateResult(n, n, "", returnList);
+    return returnList;
+  }
+
+  private void generateResult(int left, int right, String result, List<String> returnList) {
+    if (left == 0 && right == 0) {
+      returnList.add(result);
+      return;
+    }
+    if (left > 0) {
+      generateResult(left - 1, right, result + "(", returnList);
+    }
+    if (right >  left) {
+      generateResult(left, right - 1, result + ")", returnList);
+    }
+  }
+
+  public static void main(String[] args) {
+    GenerateParentheses generateParentheses = new GenerateParentheses();
+    generateParentheses.generateParenthesis(2);
+  }
 }
