9a236c7130bd35d632c037b93dfe49c27c6a0c48
suidonglei <suidonglei@jd.com>
2019-08-28 17:45:58 +0800

    sudoku

---
+++ src/main/java/learn/algorithm/Sudoku/validSudoku.java
@@ -0,0 +1,21 @@
+package learn.algorithm.Sudoku;
+
+/**
+ * @author suidonglei
+ * @title: validSudoku
+ * @projectName algorithm-test
+ * @description: 36
+ *
+ * Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+ *
+ * Each row must contain the digits 1-9 without repetition.
+ * Each column must contain the digits 1-9 without repetition.
+ * Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+ *
+ * @date 2019/8/2815:43
+ */
+public class validSudoku {
+  public boolean isValidSudoku(char[][] board) {
+    return false;
+  }
+}
