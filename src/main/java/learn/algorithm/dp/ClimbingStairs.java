f56b7958cb52a5a43d0adb54e5e401475aa74654
suidonglei <suidonglei@jd.com>
2019-10-12 16:20:22 +0800

    test

---
+++ src/main/java/learn/algorithm/dp/ClimbingStairs.java
@@ -0,0 +1,19 @@
+package learn.algorithm.dp;
+
+/**
+ * @author suidonglei
+ * @title: ClimbingStairs
+ * @projectName algorithm-test
+ * @description: 70.Climbing Stairs
+ *
+ * You are climbing a stair case. It takes n steps to reach to the top.
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ * Note: Given n will be a positive integer.
+ *
+ * @date 2019/10/11 14:47
+ */
+public class ClimbingStairs {
+  public int climbStairs(int n) {
+
+  }
+}
