903c616bc0a7216c86fce1e2f788501453877753
suidonglei <suidonglei@jd.com>
2019-10-22 15:30:52 +0800

    dp program

--- src/main/java/learn/algorithm/dp/ClimbingStairs.java
+++ src/main/java/learn/algorithm/dp/ClimbingStairs.java
@@ -14,7 +14,18 @@ package learn.algorithm.dp;
  */
 public class ClimbingStairs {
   public int climbStairs(int n) {
-
-      return 10;
+      if(n == 0 || n == 1 || n == 2) return n;
+      int[] result = new int[n];
+      result[0] = 1;
+      result[1] = 2;
+      for(int i = 2; i < n; i++) {
+        result[i] = result[i-1] + result[i-2];
+      }
+      return result[n-1];
   }
+
+    public static void main(String[] args) {
+        ClimbingStairs climbingStairs = new ClimbingStairs();
+        System.out.println(climbingStairs.climbStairs(44));
+    }
 }
