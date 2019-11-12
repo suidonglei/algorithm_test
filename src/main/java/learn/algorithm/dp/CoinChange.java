9ae0573ecba49d72d38b54b65f5c601f408dd3d1
suidonglei <suidonglei@jd.com>
2019-11-12 20:05:38 +0800

    dp program

---
+++ src/main/java/learn/algorithm/dp/CoinChange.java
@@ -0,0 +1,14 @@
+package learn.algorithm.dp;
+
+/**
+ * 322. Coin Change
+ *
+ * You are given coins of different denominations and a total amount of money amount.
+ * Write a function to compute the fewest number of coins that you need to make up that amount.
+ * If that amount of money cannot be made up by any combination of the coins, return -1.
+ */
+public class CoinChange {
+    public int coinChange(int[] coins, int amount) {
+
+    }
+}
