8ae3d41d7f06b7823bb17424df3dadedd2308bbc
suidonglei <suidonglei@jd.com>
2019-11-14 14:34:29 +0800

    dp program

--- src/main/java/learn/algorithm/dp/CoinChange.java
+++ src/main/java/learn/algorithm/dp/CoinChange.java
@@ -9,6 +9,40 @@ package learn.algorithm.dp;
  */
 public class CoinChange {
     public int coinChange(int[] coins, int amount) {
+        if(null == coins || coins.length < 1) return -1;
+        if(amount < 1) return -1;
+        int[] changes = new int[amount + 1];
+        for(int i = 1; i < changes.length; i ++) {
+            changes[i] = -1;
+        }
+        for (int i = 1; i <= amount; i ++) {
+            int min = Integer.MAX_VALUE;
+            for(int j = 0; j < coins.length; j ++) {
+                if (i - coins[j] >= 0 && changes[i - coins[j]] != -1 && changes[i - coins[j]] < min) {
+                    min = changes[i - coins[j] ];
+                }
+            }
+            if(min != Integer.MAX_VALUE) changes[i] = min + 1;
+        }
+        return changes[amount];
+    }
 
+    public int coinChange1(int[] coins, int amount) {
+        if(null == coins || coins.length < 1) return -1;
+        if(amount < 1) return -1;
+        int[] changes = new int[amount + 1];
+        for(int i = 1; i < changes.length; i ++) {
+            changes[i] = -1;
+        }
+        for (int i = 1; i <= amount; i ++) {
+            int min = Integer.MAX_VALUE;
+            for(int j = 0; j < coins.length; j ++) {
+                if (i - coins[j] >= 0 && changes[i - coins[j]] != -1 && changes[i - coins[j]] < min) {
+                    min = changes[i - coins[j] ];
+                }
+            }
+            if(min != Integer.MAX_VALUE) changes[i] = min + 1;
+        }
+        return changes[amount];
     }
 }
---
+++ src/main/java/learn/algorithm/dp/EditDistance.java
@@ -0,0 +1,4 @@
+package learn.algorithm.dp;
+
+public class EditDistance {
+}
