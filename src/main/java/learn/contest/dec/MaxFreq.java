00be23fec275ed0060a532bb0869cfe7025a0933
suidonglei <suidonglei@jd.com>
2019-12-24 21:44:40 +0800

    greedy algorithm

--- src/main/java/learn/contest/dec/MaxFreq.java
+++ src/main/java/learn/contest/dec/MaxFreq.java
@@ -2,6 +2,10 @@ package learn.contest.dec;
 
 /**
  * 1297. Maximum Number of Occurrences of a Substring
+ * Given a string s, return the maximum number of ocurrences of any substring under the following rules:
+ *
+ * The number of unique characters in the substring must be less than or equal to maxLetters.
+ * The substring size must be between minSize and maxSize inclusive.
  */
 public class MaxFreq {
     public int maxFreq(String s, int maxLetters, int minSize, int maxSize) {
